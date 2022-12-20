81a6c15f5891c290b7206cd7fbd9bdb3f77aa810
ShreeThannu <shree.thaanu@thoughtworks.com>
2022-12-20 15:58:25 +0530

    [Ravi|ShreeThaanu] [iOS] changed salt

--- ios/bluetoothdemo/bluetoothdemo/cryptography/Utils/KeyGenerator.swift
+++ ios/bluetoothdemo/bluetoothdemo/cryptography/Utils/KeyGenerator.swift
@@ -11,9 +11,9 @@ import CryptoKit
 class KeyGenerator {
 
     func generateStrongKeyBasedOnHKDF(sharedSecretKey: SharedSecret, keyLength: Int, infoData: String) -> SymmetricKey {
-        let salt = Data([0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c])
+        let salt = "SHA-256".data(using: .utf8)
         let sharedInfo = infoData.data(using: .utf8)
-        let strongKey = sharedSecretKey.hkdfDerivedSymmetricKey(using: SHA256.self, salt: salt, sharedInfo: sharedInfo!, outputByteCount: keyLength)
+        let strongKey = sharedSecretKey.hkdfDerivedSymmetricKey(using: SHA256.self, salt: salt!, sharedInfo: sharedInfo!, outputByteCount: keyLength)
         return strongKey
     }
 }
