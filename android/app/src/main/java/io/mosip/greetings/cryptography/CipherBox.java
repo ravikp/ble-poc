10ade4396b3f82ad76244d97c73ce4b07cf2d4bd
Ravi Kumar P <ravi.pasumarthy@gmail.com>
2022-12-20 14:06:48 +0530

    [Ravi] [android] Include debug statements temporarily to communication between android and iOS

--- android/app/src/main/java/io/mosip/greetings/cryptography/CipherBox.java
+++ android/app/src/main/java/io/mosip/greetings/cryptography/CipherBox.java
@@ -1,6 +1,7 @@
 package io.mosip.greetings.cryptography;
 
 interface CipherBox {
+    void printSecretKey(String identifier);
     byte[] encrypt(byte[] plainText);
     byte[] decrypt(byte[] cipherText);
 }
