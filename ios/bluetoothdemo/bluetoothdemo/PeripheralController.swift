9dbe4fc7b4fb92c3e8b69441b3da9bac45c498b5
Harsh Vardhan <harsh.vardhan1@thoughtworks.com>
2023-01-03 09:27:55 +0530

    [Harsh] Set local device name to byte array

--- ios/bluetoothdemo/bluetoothdemo/PeripheralController.swift
+++ ios/bluetoothdemo/bluetoothdemo/PeripheralController.swift
@@ -60,7 +60,8 @@ class PeripheralController: NSObject, ObservableObject {
 
     private func startAdvertising() {
         // not setting Local name via CBAdvertisementDataLocalNameKey
-        peripheralManager.startAdvertising([CBAdvertisementDataServiceUUIDsKey: relevantPeripherals])
+        peripheralManager.startAdvertising([CBAdvertisementDataServiceUUIDsKey: relevantPeripherals, CBAdvertisementDataLocalNameKey: Data([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+                                                                                    14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32])])
     }
 
     func sendData(message: String) {
